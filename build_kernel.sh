7cac47793b26205c108fb3648ad9e7d51cbfc76a
Stefan Agner <stefan@agner.ch>
2018-05-08 22:50:38 +0100

    ARM: 8767/1: add support for building ARM kernel with clang
    (commit c1c386681bd73c4fc28eb5cc91cf8b7be9b409ba upstream)
    
    Use cc-options call for compiler options which are not available
    in clang. With this patch an ARMv7 multi platform kernel can be
    successfully build using clang (tested with version 5.0.1).
    
    Based-on-patches-by: Behan Webster <behanw@converseincode.com>
    
    Signed-off-by: Stefan Agner <stefan@agner.ch>
    Signed-off-by: Russell King <rmk+kernel@armlinux.org.uk>
    Signed-off-by: Nick Desaulniers <ndesaulniers@google.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- build_kernel.sh
+++ build_kernel.sh
@@ -13,7 +13,7 @@ KERNEL_LLVM_BIN=/home/flatty/6.0/bin/clang
 KERNEL_LLVM_CFP=/home/flatty/6.0-cfp/bin/clang
 #BUILD_CROSS_COMPILE=$BUILD_TOP_DIR/PLATFORM/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-android-
 #KERNEL_LLVM_BIN=$BUILD_TOP_DIR/toolchain/llvm-arm-toolchain-ship/6.0/bin/clang
-CLANG_TRIPLE=/home/flatty/aarch64-linux-gnu
+CLANG_TRIPLE=aarch64-linux-gnu-
 KERNEL_MAKE_ENV="DTC_EXT=$(pwd)/tools/dtc CONFIG_BUILD_ARM64_DT_OVERLAY=y"
 PROC="$(($(nproc) + 1))"
 test -z "$ANDROID_VERSION" && ANDROID_VERSION=990000 # qwerty12

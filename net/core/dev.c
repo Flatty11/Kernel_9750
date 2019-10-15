31301fc6565652473fef0ba22df6e2e56f58147e
Flatty11 <kosov11@gmail.com>
2019-10-15 19:39:26 +0300

    Revert crypto-gcm patch - doesn't boot

--- net/core/dev.c
+++ net/core/dev.c
@@ -7914,7 +7914,6 @@ static void netdev_wait_allrefs(struct net_device *dev)
 
 		refcnt = netdev_refcnt_read(dev);
 
-<<<<<<< HEAD
 		if (time_after(jiffies, warning_time + 5 * HZ)) {
 			/*
 			 * rmnet dev hold will not move on to next creation,
